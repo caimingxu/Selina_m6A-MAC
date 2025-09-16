13f8c22acd16725eb296e7f0ff46d85c07525bb6
caimingxu <94650613+caimingxu@users.noreply.github.com>
2025-09-16 14:38:09 -0700

    Create violinPlot.R

---
+++ R/1_Discovery_TCGA/violinPlot.R
@@ -0,0 +1,14 @@
+library(sm)
+library(zoo)
+library(ggplot2)
+library(vioplot)
+
+rt <- read.table("input.txt",header = T,sep = "\t",check.names = F)
+ggplot(rt, aes(x = rt$group, y = rt$exp)) + 
+  geom_violin(aes(fill = rt$group), trim = FALSE) +
+  geom_boxplot(width = 0.1) +
+  theme_classic() +
+  theme(legend.position = "none")
+
+library(export)
+graph2ppt(file="violin.pptx", width=5, height=5)
